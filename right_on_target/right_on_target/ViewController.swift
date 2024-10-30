026c013584fba17f3af3ed11f361db727bf025cc
Daniil <danast_2@mail.ru>
2024-10-30 20:28:34 +0300

    new

---
+++ right_on_target/right_on_target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  right_on_target
+//
+//  Created by Даниил Асташов on 30.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
