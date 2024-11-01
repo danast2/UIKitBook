34559d1bd26275d96d21f675f301a34d6e250aba
Daniil <danast_2@mail.ru>
2024-11-01 23:14:32 +0300

    chapter_1_new

--- right_on_target/right_on_target/ViewController.swift
+++ right_on_target/right_on_target/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        
+            super.viewDidLoad()
+             print("viewDidLoad")
     }
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
