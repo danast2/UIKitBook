40e33d8ed9bd4cea61099713116dda871fc4a634
Daniil <danast_2@mail.ru>
2024-11-07 13:50:28 +0300

    chapter_3_9

---
+++ right_on_target/right_on_target/SecondViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SecondViewController.swift
+//  right_on_target
+//
+//  Created by Даниил Асташов on 07.11.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func loadView() {
+         super.loadView()
+         print("loadView SecondViewController")
+     }
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         print("viewDidLoad SecondViewController")
+     }
+
+     override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         print("viewWillAppear SecondViewController")
+     }
+
+     override func viewDidAppear(_ animated: Bool) {
+         super.viewDidAppear(animated)
+         print("viewDidAppear SecondViewController")
+     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         print("viewWillDisappear SecondViewController")
+     }
+
+     override func viewDidDisappear(_ animated: Bool) {
+         super.viewDidDisappear(animated)
+         print("viewDidDisappear SecondViewController")
+     }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
