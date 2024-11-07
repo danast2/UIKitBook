e8d112f015bcba9e83495da16799e00687e2f0c2
Daniil <danast_2@mail.ru>
2024-11-07 13:45:26 +0300

    chapter_3_8

--- right_on_target/right_on_target/ViewController.swift
+++ right_on_target/right_on_target/ViewController.swift
@@ -17,6 +17,16 @@ class ViewController: UIViewController {
     // сумма очков за раунд
     var points: Int = 0
     
+    
+    override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+         super.viewDidDisappear(animated)
+         print("viewDidDisappear")
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
          super.viewWillAppear(animated)
          print("viewWillAppear")
