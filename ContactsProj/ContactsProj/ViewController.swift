cda71b53faddf6e00cdd851b65ccacd2bd294113
Daniil <danast_2@mail.ru>
2024-11-24 09:11:47 +0300

    24.11.24_1

--- ContactsProj/ContactsProj/ViewController.swift
+++ ContactsProj/ContactsProj/ViewController.swift
@@ -21,15 +21,16 @@ extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
         return 50
     }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // получаем экземпляр ячейки
+       let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+       // конфигурируем ячейку
+       var configuration = cell.defaultContentConfiguration()
+       configuration.text = "Строка \(indexPath.row)"
+       cell.contentConfiguration = configuration
+       // возвращаем сконфигурированный экземпляр ячейки
+       return cell
+    }
 }
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         // получаем экземпляр ячейки
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        // конфигурируем ячейку
-        var configuration = cell.defaultContentConfiguration()
-        configuration.text = "Строка \(indexPath.row)"
-        cell.contentConfiguration = configuration
-        // возвращаем сконфигурированный экземпляр ячейки
-        return cell
-     }
+     
 
