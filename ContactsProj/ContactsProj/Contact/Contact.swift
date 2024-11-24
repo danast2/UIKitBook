259fd97b8fb3f28c373a0deb7e95288a2482327e
Daniil <danast_2@mail.ru>
2024-11-25 00:38:44 +0300

    25.11.24_2

--- ContactsProj/ContactsProj/Contact/Contact.swift
+++ ContactsProj/ContactsProj/Contact/Contact.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+protocol ContactProtocol {
+ /// Имя
+ var title: String { get set }
+ /// Номер телефона
+ var phone: String { get set }
+}
+struct Contact: ContactProtocol {
+ var title: String
+ var phone: String
+}
--- ContactsProj/ContactsProj/ViewController.swift
+++ ContactsProj/ContactsProj/ViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var contacts = [ContactProtocol]()
+    private func loadContacts() {
+     contacts.append(Contact(title: "Саня Техосмотр", phone: "+799912312323"))
+     contacts.append(Contact(title: "Владимир Анатольевич", phone: "+781213342321"))
+     contacts.append(Contact(title: "Сильвестр", phone: "+7000911112"))
+     contacts.sort{ $0.title < $1.title }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadContacts()
         // Do any additional setup after loading the view.
     }
-    private func configure(cell: inout UITableViewCell, for indexPath:IndexPath) {
+    private func configure(cell: inout UITableViewCell, for indexPath:
+    IndexPath) {
      var configuration = cell.defaultContentConfiguration()
-     configuration.text = "Строка \(indexPath.row)"
+     // имя контакта
+     configuration.text = contacts[indexPath.row].title
+     // номер телефона контакта
+     configuration.secondaryText = contacts[indexPath.row].phone
      cell.contentConfiguration = configuration
     }
 
@@ -23,7 +34,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        return 50
+        return contacts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: UITableViewCell
