8551e01b506c8d16dc9ab1c3b704e5e997df7d12
Daniil <danast_2@mail.ru>
2024-12-04 23:18:54 +0300

    new_1

--- TransferApp/TransferApp/ViewController.swift
+++ TransferApp/TransferApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //review 04.12.24
     }
 
 
