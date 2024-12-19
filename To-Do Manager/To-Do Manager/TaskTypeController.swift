0287e0a363c266575ef422095982b343ba374dcb
Daniil <danast_2@mail.ru>
2024-12-20 01:13:15 +0300

    new_10

---
+++ To-Do Manager/To-Do Manager/TaskTypeController.swift
@@ -0,0 +1,100 @@
+//
+//  TaskTypeController.swift
+//  To-Do Manager
+//
+//  Created by Даниил Асташов on 20.12.2024.
+//
+
+import UIKit
+
+class TaskTypeController: UITableViewController {
+    // 1. кортеж, описывающий тип задачи
+     typealias TypeCellDescription = (type: TaskPriority, title: String, description: String)
+
+     // 2. коллекция доступных типов задач с их описанием
+     private var taskTypesInformation: [TypeCellDescription] = [
+     (type: .important, title: "Важная", description: "Такой тип задач является наиболее приоритетным для выполнения. Все важные задачи выводятся в самом верху списка задач"), (type: .normal, title: "Текущая", description: "Задача с обычным приоритетом")
+     ]
+     // 3. выбранный приоритет
+     var selectedType: TaskPriority = .normal
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Установка автоматической высоты строк
+        tableView.estimatedRowHeight = 44 // Оценочная высота строки
+        tableView.rowHeight = UITableView.automaticDimension
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
