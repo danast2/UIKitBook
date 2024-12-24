05cbb8bbf179433923459d4e666e6143b0d452e1
Daniil <danast_2@mail.ru>
2024-12-24 17:35:20 +0300

    new_23

---
+++ ViewController/ViewController/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Даниил Асташов on 24.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showButton(_ sender: Any) {
+        
+    }
+    
+
+}
