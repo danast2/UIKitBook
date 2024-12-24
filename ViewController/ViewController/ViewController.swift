72b87c664cacead35b7d1c4056d3e8f6bbbb437e
Daniil <danast_2@mail.ru>
2024-12-24 21:24:47 +0300

    new_24

--- ViewController/ViewController/ViewController.swift
+++ ViewController/ViewController/ViewController.swift
@@ -16,7 +16,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showButton(_ sender: Any) {
-        
+        let alertController = UIAlertController(title: "Error", message: "Ahtung, you have an alert", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .cancel) { (action) in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil text")
+        }
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
     }
     
 
