fa4ba229e170ecd2dd6156545c30ddfd9a8a69b9
Daniil <danast_2@mail.ru>
2024-12-25 22:19:22 +0300

    new_26

--- ViewController/ViewController/ViewController.swift
+++ ViewController/ViewController/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,15 +18,20 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showButton(_ sender: Any) {
+        self.hiLabel.text = "Hi"
         self.alert(title: "Ошибка", message: "что-то не так", style: .alert)
     }
     //2
     func alert(title: String, message: String, style: UIAlertController.Style){
         let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
         let action = UIAlertAction(title: "ok", style: .cancel) { (action) in
-            
+            let textAlert = alertController.textFields?.first
+            self.hiLabel.text! += (", ") + (textAlert?.text!)! + ("!")
         }
-        
+        alertController.addTextField{ (textField) in
+            textField.isSecureTextEntry = true
+        }
+        alertController.addAction(action)
         self.present(alertController, animated: true, completion: nil)
     }
     
