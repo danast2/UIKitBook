62f05f997d9b93cb4d6a172ea814ac2c56875b90
Daniil <danast_2@mail.ru>
2024-12-26 23:04:55 +0300

    new_25

---
+++ Switch/Switch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Switch
+//
+//  Created by Даниил Асташов on 26.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let mySwitch = UISwitch()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
