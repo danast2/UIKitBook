006878b82f3accad6445659dd7d6eb81405eef69
Daniil <danast_2@mail.ru>
2024-12-27 17:56:12 +0300

    new_26

--- Switch/Switch/ViewController.swift
+++ Switch/Switch/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     let mySwitch = UISwitch()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
+        self.view.addSubview(self.mySwitch)
     }
-
-
 }
-
