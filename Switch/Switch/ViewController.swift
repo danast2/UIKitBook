ac0fac66a0277da5bb055045d37f142626f0114e
Daniil <danast_2@mail.ru>
2024-12-27 18:07:45 +0300

    new_27

--- Switch/Switch/ViewController.swift
+++ Switch/Switch/ViewController.swift
@@ -16,5 +16,25 @@ class ViewController: UIViewController {
         
         self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
         self.view.addSubview(self.mySwitch)
+        
+        self.mySwitch.setOn(true, animated: true)
+        
+//        if self.mySwitch.isOn{
+//            print("switch is on")
+//        }else{
+//            print("switch is off")
+//        }
+        
+        self.mySwitch.addTarget(self, action: #selector(switchChange(paramTarget: )), for: .valueChanged)
+    }
+    
+    @objc func switchChange(paramTarget: UISwitch){
+        print("param is = ", paramTarget)
+
+        if self.mySwitch.isOn{
+            print("switch is on")
+        }else{
+            print("switch is off")
+        }
     }
 }
