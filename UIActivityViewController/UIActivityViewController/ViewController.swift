3d61cc1a93b51e2dbdb7b622e9e0b828aedcc616
Daniil <danast_2@mail.ru>
2025-01-01 19:57:48 +0300

    new_37

--- UIActivityViewController/UIActivityViewController/ViewController.swift
+++ UIActivityViewController/UIActivityViewController/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.createTextField()
+        self.createButton()
     }
     //MARK: - Method
     func createTextField(){
@@ -23,7 +24,30 @@ class ViewController: UIViewController {
         self.textField.center = self.view.center
         self.textField.borderStyle = UITextField.BorderStyle.roundedRect
         self.textField.placeholder = "enter text to share"
+        self.view.addSubview(self.textField)
     }
-
+    
+    func createButton(){
+        self.buttonShare = UIButton(type: .roundedRect)
+        self.buttonShare.frame = CGRect(x: 50, y: 350, width: 280, height: 44)
+        self.buttonShare.setTitle("Share", for: .normal)
+        self.buttonShare.addTarget(self, action: #selector(handleShare(paramSender:)), for: .touchUpInside)
+        self.view.addSubview(self.buttonShare)
+    }
+    
+    @objc
+    func handleShare(paramSender: Any){
+        guard let text = self.textField.text, !text.isEmpty else{
+            let message = "Сначала введите текст, потом нажмите кнопку"
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        self.activityViewController = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+        self.present(self.activityViewController!, animated: true, completion: nil)
+    }
 }
 
