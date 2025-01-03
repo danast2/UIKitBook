35969a7d4d81c520c3f1f0a740079506529ea056
Daniil <danast_2@mail.ru>
2025-01-03 16:05:43 +0300

    new_42

--- UINavigationViewController_part_2/UINavigationViewController_part_2/ViewController.swift
+++ UINavigationViewController_part_2/UINavigationViewController_part_2/ViewController.swift
@@ -11,6 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem =  UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(perfAdd(param:)))
+    }
+    
+    //MARK: - Method
+    @objc func perfAdd(param: UIBarButtonItem) {
+        print("Add click")
+    }
+    
+    //MARK: - создана с помощью пкм (Refactor -> Extract to Method)
+    fileprivate func createImageTitleView() {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
         //поможет правильно отображать (чтобы не было странного растяжения)
         imageView.contentMode = .scaleAspectFit
@@ -18,7 +28,6 @@ class ViewController: UIViewController {
         imageView.image = image
         self.navigationItem.titleView = imageView
     }
-
 
 }
 
