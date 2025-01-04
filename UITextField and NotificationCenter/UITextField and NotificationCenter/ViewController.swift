5e78f322cdf2a91f004526896dd288d4bd56a6d7
Daniil <danast_2@mail.ru>
2025-01-04 11:46:52 +0300

    new_46

--- UITextField and NotificationCenter/UITextField and NotificationCenter/ViewController.swift
+++ UITextField and NotificationCenter/UITextField and NotificationCenter/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createTextField()
+        myTextField.delegate = self
         
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldTextDidChange(ncParam:)), name: UITextField.textDidChangeNotification, object: nil)
+    }
+    
+    //MARK: - Notification
+    @objc func textFieldTextDidChange(ncParam: NSNotification){
+        print("UITextFieldNotification = \(ncParam)")
+    }
+    
+    
+    
+    //MARK: - createUI
+    
+    func createTextField() {
         // Устанавливаем рамку текстового поля
         let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
         self.myTextField = UITextField(frame: textFieldFrame)
@@ -35,65 +50,67 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: - UITextFieldDelegate methods
     
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        print("Текстовое поле готово к редактированию")
+        print("textFieldShouldBeginEditing Текстовое поле готово к редактированию")
         return true // Возвращаем true, чтобы разрешить редактирование
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("Начато редактирование текстового поля")
+        print("textFieldDidBeginEditing Начато редактирование текстового поля")
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("Редактирование текстового поля скоро завершится")
+        print(" textFieldShouldEndEditing Редактирование текстового поля скоро завершится")
         return true // Возвращаем true, чтобы разрешить завершение редактирования
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        print("Редактирование текстового поля завершено")
+        print("textFieldDidEndEditing Редактирование текстового поля завершено")
     }
     
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
         switch reason {
         case .committed:
-            print("Пользователь завершил редактирование (нажал Enter)")
+            print("textFieldDidEndEditing Пользователь завершил редактирование (нажал Enter)")
         case .cancelled:
-            print("Редактирование было отменено")
+            print("textFieldDidEndEditing Редактирование было отменено")
         @unknown default:
-            print("Неизвестная причина завершения редактирования")
+            print("textFieldDidEndEditing Неизвестная причина завершения редактирования")
         }
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print("Пользователь ввел символ: \(string)")
+        print("shouldChangeCharactersIn Пользователь ввел символ: \(string)")
         return true // Возвращаем true, чтобы разрешить изменение текста
     }
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        print("Изменился выбор текста")
+        print("textFieldDidChangeSelection Изменился выбор текста")
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        print("Текстовое поле будет очищено")
+        print("textFieldShouldClear Текстовое поле будет очищено")
         return true // Возвращаем true, чтобы разрешить очистку
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("Пользователь нажал Return")
-        textField.resignFirstResponder() // Скрываем клавиатуру
+        print("textFieldShouldReturn Пользователь нажал Return")
+        if textField == myTextField{
+            textField.resignFirstResponder() // Скрываем клавиатуру
+        }
         return true
     }
     
     func textField(_ textField: UITextField, editMenuForCharactersIn range: NSRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
-        print("Вызвано контекстное меню для выделенных символов")
+        print("editMenuForCharactersIn Вызвано контекстное меню для выделенных символов")
         return nil // Возвращаем nil, чтобы использовать стандартное меню
     }
     
     func textField(_ textField: UITextField, willPresentEditMenuWith animator: any UIEditMenuInteractionAnimating) {
-        print("Контекстное меню будет показано")
+        print("willPresentEditMenuWith Контекстное меню будет показано")
     }
     
     func textField(_ textField: UITextField, willDismissEditMenuWith animator: any UIEditMenuInteractionAnimating) {
-        print("Контекстное меню будет скрыто")
+        print("willDismissEditMenuWith Контекстное меню будет скрыто")
     }
 }
 
