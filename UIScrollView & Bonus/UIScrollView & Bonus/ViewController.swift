3d7e7da8f2ad4f1c31ff8e7126c12c7bc7978e4b
Daniil <danast_2@mail.ru>
2025-01-06 00:43:40 +0300

    new_53

--- UIScrollView & Bonus/UIScrollView & Bonus/ViewController.swift
+++ UIScrollView & Bonus/UIScrollView & Bonus/ViewController.swift
@@ -10,41 +10,43 @@ import UIKit
 class ViewController: UIViewController, UIScrollViewDelegate {
     
     var myScrollView = UIScrollView()
-    var myImageView = UIImageView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        createScrollView()
+        
+        let newScreenForUIImageView = UIImage(named: "new_screen_for_UIImageView.png")
+        let vinil = UIImage(named: "vinil.png")
+        let speaker = UIImage(named: "speaker.png")
+        let scrollViewRect = self.view.bounds
+        
+        //create scroll
+        self.myScrollView = UIScrollView(frame: scrollViewRect)
+        self.myScrollView.isPagingEnabled = true
+        self.myScrollView.contentSize = CGSize(width: scrollViewRect.size.width * 3, height: scrollViewRect.size.height)
+        self.view.addSubview(self.myScrollView)
+        
+        //create screenShot
+        var imageViewRect = self.view.bounds
+        let screenShotImageView = self.newImageViewWithImage(paramImage: newScreenForUIImageView!, paramFrame: imageViewRect)
+        self.myScrollView.addSubview(screenShotImageView)
+        
+        //next page
+        imageViewRect.origin.x += imageViewRect.size.width
+        let speakerImageView = self.newImageViewWithImage(paramImage: speaker!, paramFrame: imageViewRect)
+        self.myScrollView.addSubview(speakerImageView)
+        
+        //next page
+        imageViewRect.origin.x += imageViewRect.size.width
+        let vinilImageView = self.newImageViewWithImage(paramImage: vinil!, paramFrame: imageViewRect)
+        self.myScrollView.addSubview(vinilImageView)
     }
-    // MARK: - UIScrollViewDelegate
-
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let p = "Начинается прокрутка"
-        print(p)
-        print(scrollView.contentOffset.y)
-        self.myScrollView.alpha = 0.50
-    }
-
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let p = "Вызывается после прокрутки"
-        print(p)
-        self.myScrollView.alpha = 1.0
-    }
-
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        let p = "Гарантирует, что вернет альфу к 1"
-        print(p)
-        self.myScrollView.alpha = 1.0
-    }
-
-    func createScrollView() {
-        let imageToLoad = UIImage(named: "new_screen_for_UIImageView.png")
-        self.myImageView = UIImageView(image: imageToLoad)
-        self.myScrollView = UIScrollView(frame: self.view.bounds)
-        self.myScrollView.addSubview(myImageView)
-        self.myScrollView.contentSize = self.myImageView.bounds.size
-        self.myScrollView.delegate = self
-        self.view.addSubview(myScrollView)
+    
+    func newImageViewWithImage(paramImage: UIImage, paramFrame: CGRect) -> UIImageView {
+        let result = UIImageView(frame: paramFrame)
+        result.contentMode = .scaleAspectFit
+        result.image = paramImage
+        return result
     }
 
 }
