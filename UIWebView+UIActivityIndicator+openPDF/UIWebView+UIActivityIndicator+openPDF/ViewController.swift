fb792e5e0d8231c424eecc7cd38e1efcbdc93148
Daniil <danast_2@mail.ru>
2025-01-06 23:04:13 +0300

    new_55

--- UIWebView+UIActivityIndicator+openPDF/UIWebView+UIActivityIndicator+openPDF/ViewController.swift
+++ UIWebView+UIActivityIndicator+openPDF/UIWebView+UIActivityIndicator+openPDF/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupWebView()
+        self.setupToolBar()
     }
     
     func setupWebView() {
@@ -32,5 +34,42 @@ class ViewController: UIViewController {
             self.webView.load(URLRequest(url: url))
         }
     }
+    
+    func setupToolBar() {
+        let toolbar = UIToolbar(frame: .zero)
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(toolbar)
+        
+        NSLayoutConstraint.activate([self.webView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+                                     self.webView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                     self.webView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+                                     self.webView.heightAnchor.constraint(equalToConstant: 50.0)
+                                    ])
+        // Создаем кнопки
+        let backButton = UIBarButtonItem(title: "Назад", style: .plain, target: self, action: #selector(goBack))
+        let forwardButton = UIBarButtonItem(title: "Вперед", style: .plain, target: self, action: #selector(goForward))
+        let reloadButton = UIBarButtonItem(title: "Обновить", style: .plain, target: self, action: #selector(reloadPage))
+        let flexibleSpace = UIBarButtonItem.flexibleSpace() // Для равномерного распределения кнопок
+
+        // Добавляем кнопки на Toolbar
+        toolbar.items = [backButton, flexibleSpace, forwardButton, flexibleSpace, reloadButton]
+    }
+    
+    @objc func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+
+    @objc func goForward() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+
+    @objc func reloadPage() {
+        webView.reload()
+    }
+
 }
 
