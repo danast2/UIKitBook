9da06f8d4dbdc88a3e563f9a7d5fd9b74e6689a9
Daniil <danast_2@mail.ru>
2025-01-09 23:54:46 +0300

    new_65

---
+++ Auto Layout Guide Interface Builder/Auto Layout Guide Interface Builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Guide Interface Builder
+//
+//  Created by Даниил Асташов on 09.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
