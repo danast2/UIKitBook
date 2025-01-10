a11a606ea9a0da39f2d6f8dd09e668af24936078
Daniil <danast_2@mail.ru>
2025-01-10 23:39:20 +0300

    new_66

---
+++ Auto Layout Guide Code(VFL,NSLayout Anchor)/Auto Layout Guide Code(VFL,NSLayout Anchor)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Guide Code(VFL,NSLayout Anchor)
+//
+//  Created by Даниил Асташов on 10.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
