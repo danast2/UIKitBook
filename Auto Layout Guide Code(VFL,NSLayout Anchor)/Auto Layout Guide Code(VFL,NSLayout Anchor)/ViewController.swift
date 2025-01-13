0d9a6279d95e36d3b79f40efa693cfd20fccd527
Daniil <danast_2@mail.ru>
2025-01-13 16:47:26 +0300

    new_68

--- Auto Layout Guide Code(VFL,NSLayout Anchor)/Auto Layout Guide Code(VFL,NSLayout Anchor)/ViewController.swift
+++ Auto Layout Guide Code(VFL,NSLayout Anchor)/Auto Layout Guide Code(VFL,NSLayout Anchor)/ViewController.swift
@@ -8,13 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var view1: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        view.backgroundColor = .red
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // review 11.01.25.
-        // review 12.01.25.
+        self.view.addSubview(view1)
+        self.createViewConstraint()
     }
-
+    
+    func createViewConstraint() {
+        NSLayoutConstraint(item: view1,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .topMargin,
+                           multiplier: 1,
+                           constant: 88).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+    }
 
 }
 
