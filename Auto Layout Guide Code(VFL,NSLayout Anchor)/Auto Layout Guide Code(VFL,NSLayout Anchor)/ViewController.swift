691c26dccc8efabf1afa1d89f225a6e422dcd5c0
Daniil <danast_2@mail.ru>
2025-01-13 17:12:21 +0300

    new_70

--- Auto Layout Guide Code(VFL,NSLayout Anchor)/Auto Layout Guide Code(VFL,NSLayout Anchor)/ViewController.swift
+++ Auto Layout Guide Code(VFL,NSLayout Anchor)/Auto Layout Guide Code(VFL,NSLayout Anchor)/ViewController.swift
@@ -1,111 +1,47 @@
-//
-//  ViewController.swift
-//  Auto Layout Guide Code(VFL,NSLayout Anchor)
-//
-//  Created by Даниил Асташов on 10.01.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    var view1: UIView = {
+    var viewRed: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         view.backgroundColor = .red
         return view
     }()
     
-    var view2: UIView = {
+    var viewGreen: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        view.backgroundColor = .red
+        view.backgroundColor = .green
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(view1)
-        self.view.addSubview(view2)
-        self.createView1Constraint()
-        self.createView2Constraint()
+        
+        // Добавляем subviews
+        view.addSubview(viewRed)
+        view.addSubview(viewGreen)
+        
+        // Создаем метрики
+        let metrics = ["height": 100, "width": view.bounds.size.width / 3, "top": 200]
+        let viewVFL = ["viewRed": viewRed, "viewGreen": viewGreen]
+        
+        // Вертикальные ограничения
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewRed(height)]",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewGreen(height)]",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        
+        // Горизонтальные ограничения
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[viewRed(width)]-(50)-[viewGreen(width)]-|",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
     }
-    
-    func createView1Constraint() {
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerY,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerY,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-
-        NSLayoutConstraint(item: view1,
-                           attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 200).isActive = true
-
-        NSLayoutConstraint(item: view1,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 200).isActive = true
-
-    }
-    
-    func createView2Constraint() {
-        // view2 по центру по оси X относительно view1
-        NSLayoutConstraint(item: view2,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-
-        // Нижний край view2 привязан к верхнему краю view1 с отступом -8
-        NSLayoutConstraint(item: view2,
-                           attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: -8).isActive = true
-
-        // Ширина view2 фиксирована: 100
-        NSLayoutConstraint(item: view2,
-                           attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-
-        // Высота view2 фиксирована: 100
-        NSLayoutConstraint(item: view2,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-    }
-
-
 }
 
