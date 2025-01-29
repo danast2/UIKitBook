44eea4bc144b089bc6fe5ee640a95fdfd8bed5d6
Daniil <danast_2@mail.ru>
2025-01-29 23:05:41 +0300

    new_79

--- UITableViewController,MVC,UserDefaults/UITableViewController,MVC,UserDefaults/ViewController.swift
+++ UITableViewController,MVC,UserDefaults/UITableViewController,MVC,UserDefaults/ViewController.swift
@@ -1,33 +1,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let tableView = UITableView()
-    var allItem = ["Robbins", "Jobs", "Gilbert"]
-    var cellIdentifier = "Cell"
-
+    let tableViewController = UITableViewController(style: .plain)
+    var refresh = UIRefreshControl()
+    
+    var allTime = [Date]()
+    var cellIdentefier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         createTableView()
+        
+        self.refresh.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        self.refresh.tintColor = .red
+        
+        view.addSubview(tableViewController.tableView)
+        tableViewController.tableView.addSubview(refresh)
     }
     
-    func createTableView() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.frame = view.bounds // Добавление таблицы в представление
-        view.addSubview(tableView)
+    @objc func handleRefresh(){
+        allTime.append(Date())
+        refresh.endRefreshing()
+        let indexPathNewRow = IndexPath(row: allTime.count - 1, section: 0)
+        tableViewController.tableView.insertRows(at: [indexPathNewRow], with: .automatic)
     }
+    
+    
+    func createTableView() {
+          let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+          tableViewController.tableView = tableView
+          tableViewController.tableView.register(
+              UITableViewCell.self, forCellReuseIdentifier: cellIdentefier)
+          tableViewController.tableView.delegate = self
+          tableViewController.tableView.dataSource = self
+      }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allItem.count
+        return allTime.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        let item = allItem[indexPath.row]
-        cell.textLabel?.text = item
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentefier, for: indexPath)
+        let date = allTime[indexPath.row]
+        cell.textLabel?.text = "\(date)"
         return cell
     }
 }
