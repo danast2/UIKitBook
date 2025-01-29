c513741e943cc810992f693d2ed1a48083757c97
Daniil <danast_2@mail.ru>
2025-01-29 20:35:01 +0300

    new_77

--- UITableView/UITableView/ViewController.swift
+++ UITableView/UITableView/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
 
-        // Включаем редактирование
+        // Добавляем кнопку Edit
         navigationItem.rightBarButtonItem = editButtonItem
     }
-
+    
     // MARK: - UITableViewDataSource
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,49 +30,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    //MARK: - UITableViewDelegate
-    // Удаление строк
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
-    }
+    // MARK: - UITableViewDelegate
 
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            array.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-
-    // можно ли перемещать строку
+    // Разрешаем перемещение строк
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-    
-    //перемещение строк
+
+    // Логика перемещения строк
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let item = array[sourceIndexPath.row]
-        array.remove(at: sourceIndexPath.row)
+        let item = array.remove(at: sourceIndexPath.row)
         array.insert(item, at: destinationIndexPath.row)
     }
 
-    // Долгое нажатие: копирование текста
-    func tableView(_ tableView: UITableView, shouldShowMenuForRowAt indexPath: IndexPath) -> Bool {
-        return true
+    // Убираем иконку удаления, чтобы включить перемещение строк
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
     }
 
-    func tableView(_ tableView: UITableView, canPerformAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        if action == #selector(copy(_:)) {
-            return true
-        }
-        return false
-    }
-
-    func tableView(_ tableView: UITableView, performAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) {
-        if action == #selector(copy(_:)) {
-            let cell = tableView.cellForRow(at: indexPath)
-            let pasteBoard = UIPasteboard.general
-            pasteBoard.string = cell?.textLabel?.text
-        }
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.setEditing(editing, animated: animated)
     }
 }
 
