00e70c987845d600d041e4234362eb71e48de313
Daniil <danast_2@mail.ru>
2025-01-30 18:54:00 +0300

    SubscriptionViewModel_base_withoutNotifications

--- Recurring Payments/Recurring Payments/ViewModels/SubscriptionViewModel.swift
+++ Recurring Payments/Recurring Payments/ViewModels/SubscriptionViewModel.swift
@@ -6,3 +6,64 @@ import Foundation
 //loadSubscription
 //saveSubscription
 //calculateMonthlyCost(можно не только месячную подписку)
+
+
+class SubscriptionViewModel {
+    
+    //список подписок
+    private(set) var subscriptions: [Subscription] = []
+    
+    //хранилище подписок
+    private let storage = SubscriptionStorage()
+    
+    init() {
+        loadSubscription()
+    }
+    
+    func loadSubscription() -> Void {
+        subscriptions = storage.load()
+    }
+    
+    func saveSubscriptions() -> Void {
+        storage.save(subscriptions)
+    }
+    
+    func addSubscription(_ subscription: Subscription) -> Void {
+        subscriptions.append(subscription)
+        saveSubscriptions()
+        
+        //создание напоминания перед списанием
+        //........................................
+    }
+    
+    func removeSubscription(at index: Int) -> Void {
+        let subscription = subscriptions.remove(at: index)
+        saveSubscriptions()
+        
+        //удаление уведомления
+        //..................................
+    }
+    
+    
+    //подсчет стоимости подписок за месяц
+    //можно добавить рассчет на год / пол месяца / неделю и тд
+    func calculateMonthlyCost() -> Double {
+        var totalCost: Double = 0
+        
+        for subscription in subscriptions {
+            switch subscription.cycle {
+            case .monthly:
+                totalCost += subscription.price
+            case .yearly:
+                totalCost += subscription.price / 12
+            case .weekly:
+                totalCost += subscription.price * 4
+            case .halthOfMonth:
+                totalCost += subscription.price * 2
+            case .halthOfYear:
+                totalCost += subscription.price / 6
+            }
+        }
+        return totalCost
+    }
+}
