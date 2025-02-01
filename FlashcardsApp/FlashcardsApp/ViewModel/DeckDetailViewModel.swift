a260ae06bbed374cab78f7467fd69ab1de079b66
Daniil <danast_2@mail.ru>
2025-02-01 18:47:58 +0300

    DeckDetailViewModel

--- FlashcardsApp/FlashcardsApp/ViewModel/DeckDetailViewModel.swift
+++ FlashcardsApp/FlashcardsApp/ViewModel/DeckDetailViewModel.swift
@@ -1,3 +1,24 @@
 //Логика карточек в колоде
 
 import Foundation
+
+class DeckDetailViewModel: ObservableObject {
+    @Published var deck: Deck
+    
+    init(deck: Deck) {
+        self.deck = deck
+    }
+    
+    func addCard(front: String, back: String) -> Void {
+        let newCard = Card(id: UUID(), frontText: front, backText: back, reviewDate: Date(), difficulty: 3)
+        deck.cards.append(newCard)
+    }
+    
+    func deleteCard(at index: Int) -> Void {
+        deck.cards.remove(at: index)
+    }
+    
+    func getCards() -> [Card] {
+        return deck.cards
+    }
+}
